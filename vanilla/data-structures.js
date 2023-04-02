fe67fa3480f3f29550b8cab2d6825e497088738e
jadilovic <j.adilovic@gmail.com>
2023-04-02 16:21:42 +0200

    fixing promises

--- vanilla/data-structures.js
+++ vanilla/data-structures.js
@@ -45,3 +45,130 @@ const isAnagram = (word1, word2) => {
 };
 
 console.log(isAnagram('anagram', 'nagafram'));
+
+console.log('-------------------------------------------------------');
+const nums = [1, 7, 11, 44, 3, 15, 2];
+
+const findIndexes = (nums, num) => {
+	// for (let i = 0; i < nums.length; i++) {
+	// 	for (let j = i; j < nums.length; j++) {
+	// 		if (nums[i] + nums[j] === num) return [i, j];
+	// 	}
+	// }
+	// return -1;
+	const obj = {};
+	// let first = [];
+	for (let i = 0; i < nums.length; i++) {
+		const key = nums[i];
+		if (obj[num - key]) {
+			return [obj[num - key], i];
+		} else {
+			obj[key] = i;
+		}
+	}
+
+	// for (const key in obj) {
+	// 	if (obj[num - Number(key)]) {
+	// 		first.push(obj[num - Number(key)]);
+	// 	}
+	// }
+
+	return first;
+};
+
+console.log(findIndexes(nums, 9));
+
+console.log('-----------------------------------');
+
+const stockTime = (arr) => {
+	let maxProfit = 0;
+	let min = arr[0];
+	for (let i = 0; i < arr.length; i++) {
+		// for (let j = i + 1; j < arr.length; j++) {
+		// 	if (arr[j] - arr[i] > maxProfit) {
+		// 		maxProfit = arr[j] - arr[i];
+		// 	}
+		// }
+		if (arr[i] < min) min = arr[i];
+		if (arr[i] - min > maxProfit) maxProfit = arr[i] - min;
+	}
+	return maxProfit;
+};
+
+const stocks = [7, 1, 5, 3, 6, 4];
+const stocks2 = [7, 5, 3, 2, 1];
+
+console.log(stockTime(stocks));
+
+console.log('============================================================');
+
+console.log('start');
+
+const likeVideo = (video) => {
+	setTimeout(() => {
+		console.log(`You liked ${video}`);
+	}, 2000);
+};
+
+const importantFunction = (message, cb, cb2) => {
+	setTimeout(() => {
+		cb(`Timeout finished: ${message}`, cb2('top gun'));
+	}, 1);
+};
+
+const message = importantFunction(
+	'Hello',
+	(message) => {
+		console.log(message);
+	},
+	likeVideo
+);
+
+const toDo = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve(likeVideo);
+	}, 3000);
+});
+
+const toDo2 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve(likeVideo);
+	}, 1000);
+});
+
+// toDo.then((cb) => cb('terminator'));
+
+const takeCare = async (name) => {
+	const cb = await toDo;
+	cb(name + 1);
+	const cb2 = await toDo2;
+	cb2(name + 2);
+};
+
+takeCare('matcho');
+
+console.log('end');
+
+console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++');
+
+const loadJson = async (url) => {
+	// try {
+	// 	const response = await fetch(url);
+	// 	const data = await response.json();
+	// 	console.log(data);
+	// 	return data;
+	// } catch (error) {
+	// 	console.log(error.message);
+	// }
+	const response = await fetch(url);
+	if (response.status === 200) {
+		const data = await response.json();
+		return data;
+	} else {
+		throw new Error(response.status);
+	}
+};
+
+loadJson('https://fakeurl.com/no-such-user.json').catch((error) => {
+	console.log(error.message);
+});
