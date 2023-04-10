d0f19e9bc55f375fd9fb415983e28eb2f5326fb5
jadilovic <j.adilovic@gmail.com>
2023-04-10 22:03:56 +0200

    vanilla js stuff

--- modern-web/index.js
+++ modern-web/index.js
@@ -17,3 +17,68 @@ incrBtn.addEventListener('click', () => {
 	increment();
 	h1Count.textContent = count;
 });
+
+console.log('index.js');
+
+const add = (num1) => {
+	return (num2) => {
+		if (num2) return add(num1 + num2);
+		return num1;
+	};
+};
+
+console.log(add(6)(4)(3)());
+
+const calc = {
+	total: 0,
+	add(val) {
+		this.total += val;
+		return this;
+	},
+	subtract(val) {
+		this.total -= val;
+		return this;
+	},
+	multi(val) {
+		this.total *= val;
+		return this;
+	},
+	divide(val) {
+		this.total /= val;
+		return this;
+	},
+};
+
+console.log(calc.add(5).multi(4).divide(10).subtract(1));
+console.log(calc.total);
+
+document.querySelector('.products').addEventListener('click', (event) => {
+	console.log(event.target.textContent);
+	if (event.target.tagName === 'LI') {
+		window.location.href += `#${event.target.id}`;
+	}
+});
+
+const arr = [
+	[1, 2, 3],
+	[4, 5, 6, [99, 88]],
+	[7, 8, 9],
+];
+
+const newArr = arr.flat(1);
+console.log(newArr);
+
+const recursion = (arr) => {
+	const result = [];
+	if (Array.isArray(arr)) {
+	}
+};
+
+const doSomething = () => {
+	console.log('doing something');
+};
+
+const myButton = document.getElementById('myButton');
+myButton.onclick = doSomething;
+
+console.log(arrTwo);
--- modern-web/utils.js
+++ modern-web/utils.js
@@ -46,3 +46,208 @@ const sky = new Sky({
 
 sky.display();
 console.log(sky.image);
+function getFruits(fruitsList, fruitsL, fruits, ...spreading) {
+	return [...fruitsList, ...fruitsL, ...spreading, fruits];
+}
+
+console.log(getFruits(['apple', 'peach'], ['test', 'mark'], 'orange', 'lemon'));
+
+const value = { age: 10, balance: 100 };
+
+function multiply(x = { ...value }) {
+	console.log((x.age *= 2), (x.balance *= 3));
+}
+
+multiply();
+multiply();
+multiply(value);
+multiply(value);
+
+// const objValueDeep = Object.assign({}, value);
+const objValueDeep = JSON.parse(JSON.stringify(value));
+console.log((objValueDeep.age = 43));
+console.log(value);
+
+const calc = {
+	total: 0,
+	add(value) {
+		this.total += value;
+		return this.total;
+	},
+	subtract(value) {
+		this.total -= value;
+		return this.total;
+	},
+};
+
+console.log(calc.add(4));
+
+const user = {
+	name: 'Boby',
+	print() {
+		console.log(this);
+		const display = () => {
+			console.log(this);
+		};
+		display();
+	},
+	// display: () => {
+	// 	console.log(this);
+	// },
+};
+
+console.log((this.name = 'Aki'));
+console.log(this);
+user.print();
+// user.display();
+
+class Person {
+	firstName = 'Jasmin';
+	constructor(test) {
+		this.test = test;
+	}
+	print() {
+		const test = 'Linda';
+		console.log(this.firstName);
+	}
+}
+
+const indi = new Person('Mark');
+indi.print();
+
+function makeUser() {
+	return {
+		name: 'Sunny',
+		ref() {
+			return this;
+		},
+	};
+}
+
+const buddy = makeUser();
+console.log(buddy.ref().name);
+
+const guy = {
+	last: 'Check',
+	makeIt() {
+		console.log(this.last);
+	},
+};
+
+setTimeout(() => {
+	guy.makeIt();
+}, 1000);
+
+const girl = {
+	name: 'Alina',
+	display() {
+		console.log(`Hello ${this.name}`);
+		const print = () => {
+			console.log(`Byby ${this.name}`);
+		};
+		print();
+	},
+	// print: () => {
+	// 	console.log(`Byby ${this.name}`);
+	// },
+};
+
+girl.display();
+girl.display();
+
+const calculator = {
+	// a: 0,
+	// b: 0,
+	// sum: 0,
+	read() {
+		// this.a = 4;
+		// this.b = 5;
+		this.a = +prompt('a = ', 0);
+		this.b = +prompt('b = ', 0);
+	},
+	sum() {
+		return Number(this.a) + Number(this.b);
+	},
+	multi() {
+		return this.a * this.b;
+	},
+};
+
+// calculator.read();
+// console.log(calculator.sum());
+// console.log(calculator.multi());
+
+function callBack() {
+	console.log(this.city);
+}
+
+const globe = {
+	city: 'Detroit',
+	show(fn) {
+		console.log(this.city);
+		fn();
+	},
+	display() {
+		console.log(arguments);
+		console.log(arguments.length);
+		arguments[0]();
+	},
+};
+
+globe.show(callBack);
+globe.display(callBack, 1, 2, 3);
+
+const calc2 = {
+	total: 0,
+	add(value) {
+		this.total += value;
+		return this;
+	},
+	subtract(value) {
+		this.total -= value;
+		return this;
+	},
+	multi(value) {
+		this.total *= value;
+		return this;
+	},
+	divide(value) {
+		this.total /= value;
+		return this;
+	},
+};
+
+calc2.add(9);
+console.log(calc2.divide(3));
+console.log(calc2.subtract(1).add(5).total);
+
+const culmsySquare = () => {
+	const res = {};
+	return function test(num1, num2) {
+		console.log(res);
+		const attribute = num1 + ' - ' + num2;
+		if (res[attribute]) {
+			return res[attribute];
+		}
+		for (let i = 0; i < 111111111; i++) {}
+		res[attribute] = num1 * num2;
+		return res[attribute];
+	};
+};
+
+let startTime = new Date().getTime();
+const getTest = culmsySquare();
+console.log(getTest(4567, 9876));
+console.log(new Date().getTime() - startTime);
+
+startTime = new Date().getTime();
+console.log(getTest(4567, 9876));
+console.log(new Date().getTime() - startTime);
+
+startTime = new Date().getTime();
+console.log(getTest(4561, 9876));
+console.log(new Date().getTime() - startTime);
+
+startTime = new Date().getTime();
+console.log(getTest(4567, 9876));
+console.log(new Date().getTime() - startTime);
